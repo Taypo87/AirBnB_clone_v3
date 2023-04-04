26773db8544a5d073123d4be3dc4fd123a6b4843
Taypo87 <taylor.poczynek87@gmail.com>
2023-04-04 09:26:39 -0700

    fixed typos and pep8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,7 +4,8 @@
 from api.v1.views import app_views
 from flask import jsonify, request, abort, Flask, make_response
 from models import storage
-from models.amentities import Amentities
+from models.amenities import Amenities
+
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities(state_id=None):
@@ -13,13 +14,14 @@ def get_amenities(state_id=None):
 
     if request.method == 'GET':
         if state_id is None:
-            return jsonify([amenity.to_dict() for amenity in amenities.values()])
+            return jsonify([amenity.to_dict()
+                            for amenity in amenities.values()])
 
 
 @app_views.route('/amenities', methods={'POST'}, strict_slashes=False)
 def create_amenity():
     """creates a new amenity object"""
-     amenity_data = request.get_json()
+    amenity_data = request.get_json()
     if amenity_data is None:
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     if 'name' not in amenity_data.keys():
@@ -28,26 +30,28 @@ def create_amenity():
     storage.save()
     return make_response(jsonify(new_amenity.to_dict()), 201)
 
+
 @app_views.route('/amenities/<amenity_id>', methods=['GET'],
-                    strict_slashes=False)
-    def get_amenity(amenity_id=None):
-        """gets a single amenity object based on ID"""
-        amenity = storage.get(Amenity, amenity_id)
-        if amenity is None:
-            abort(404)
-        return jsonify(amenity.to_dict())
+                 strict_slashes=False)
+def get_amenity(amenity_id=None):
+    """gets a single amenity object based on ID"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
-    def delete_amenity(amenity_id=None):
-        """deletes a single amenity object based on id"""
-        amenity = storage.get(Amenity, amenity_id)
-        if amenity is None:
-            abort(404)
-        storage.delete(amenity)
-        storage.save()
-        return make_response(jsonify({}), 200)
+def delete_amenity(amenity_id=None):
+    """deletes a single amenity object based on id"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
