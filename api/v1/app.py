ff8ac3ce928d24b70ecf5197d9daab541efcf1c7
vigobin <vgobin@outlook.com>
2023-04-04 19:56:08 +0000

    Added CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,14 @@
 """ Status of your API """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from os import getenv
 from models import storage
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
